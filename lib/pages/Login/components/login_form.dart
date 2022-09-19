dd2cc9dde3121dbd94bb11cb71e11bf85029babd
Arya <64455337+H-A-L-F@users.noreply.github.com>
2022-09-19 16:30:02 +0700

    login done

--- lib/pages/Login/components/login_form.dart
+++ lib/pages/Login/components/login_form.dart
@@ -1,17 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:myanimex/constants.dart';
 
-class LoginForm extends StatelessWidget {
+class LoginForm extends StatefulWidget {
   const LoginForm({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<LoginForm> createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
   Widget build(BuildContext context) {
     return Form(
+      key: _formKey,
       child: Column(
         children: [
           TextFormField(
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please provide an email';
+              }
+              return null;
+            },
             keyboardType: TextInputType.emailAddress,
             textInputAction: TextInputAction.next,
             cursorColor: colPrimaryBase,
@@ -29,6 +43,18 @@ class LoginForm extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.symmetric(vertical: defPad),
             child: TextFormField(
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please provide a password';
+                }
+                if (value.length < 8) {
+                  return 'Password must be atleast 8 characters';
+                }
+                if (!value.contains(RegExp('^[a-zA-Z0-9_]*'))) {
+                  return 'Password must be alphanumeric';
+                }
+                return null;
+              },
               textInputAction: TextInputAction.done,
               obscureText: true,
               cursorColor: colPrimaryBase,
@@ -45,7 +71,12 @@ class LoginForm extends StatelessWidget {
           ),
           const SizedBox(height: defPad),
           ElevatedButton(
-            onPressed: () {},
+            onPressed: () {
+              if (_formKey.currentState!.validate()) {
+                // on success
+
+              }
+            },
             child: Text(
               "Login".toUpperCase(),
             ),
