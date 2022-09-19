01a87c1e0c96adada981571daffb80f705a7665a
Arya <64455337+H-A-L-F@users.noreply.github.com>
2022-09-19 14:16:20 +0700

    thrmr

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:myanimex/constants.dart';
 import 'package:myanimex/route_generator.dart';
+import 'package:adaptive_theme/adaptive_theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,35 +12,67 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-          primaryColor: colPrimaryBase,
-          scaffoldBackgroundColor: Colors.white,
-          elevatedButtonTheme: ElevatedButtonThemeData(
-            style: ElevatedButton.styleFrom(
-              elevation: 0,
-              backgroundColor: colPrimaryBase,
-              shape: const StadiumBorder(),
-              maximumSize: const Size(double.infinity, 56),
-              minimumSize: const Size(double.infinity, 56),
-            ),
+    return AdaptiveTheme(
+      light: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.red)
+            .copyWith(secondary: Colors.amber, brightness: Brightness.light),
+        scaffoldBackgroundColor: Colors.white,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            elevation: 0,
+            backgroundColor: colPrimaryBase,
+            shape: const StadiumBorder(),
+            maximumSize: const Size(double.infinity, 56),
+            minimumSize: const Size(double.infinity, 56),
           ),
-          inputDecorationTheme: const InputDecorationTheme(
-            filled: true,
-            fillColor: colPrimaryContent,
-            iconColor: colPrimaryBase,
-            prefixIconColor: colPrimaryBase,
-            contentPadding:
-                EdgeInsets.symmetric(horizontal: defPad, vertical: defPad),
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.all(Radius.circular(30)),
-              borderSide: BorderSide.none,
-            ),
-          )),
-      initialRoute: '/',
-      onGenerateRoute: RouteGenerator.generateRoute,
+        ),
+        inputDecorationTheme: const InputDecorationTheme(
+          filled: true,
+          fillColor: colPrimaryContent,
+          iconColor: colPrimaryBase,
+          prefixIconColor: colPrimaryBase,
+          contentPadding:
+              EdgeInsets.symmetric(horizontal: defPad, vertical: defPad),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(30)),
+            borderSide: BorderSide.none,
+          ),
+        ),
+      ),
+      dark: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.red)
+            .copyWith(secondary: Colors.amber, brightness: Brightness.dark),
+        scaffoldBackgroundColor: Colors.white,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            elevation: 0,
+            backgroundColor: colPrimaryBase,
+            shape: const StadiumBorder(),
+            maximumSize: const Size(double.infinity, 56),
+            minimumSize: const Size(double.infinity, 56),
+          ),
+        ),
+        inputDecorationTheme: const InputDecorationTheme(
+          filled: true,
+          fillColor: colPrimaryContent,
+          iconColor: colPrimaryBase,
+          prefixIconColor: colPrimaryBase,
+          contentPadding:
+              EdgeInsets.symmetric(horizontal: defPad, vertical: defPad),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(30)),
+            borderSide: BorderSide.none,
+          ),
+        ),
+      ),
+      initial: AdaptiveThemeMode.light,
+      builder: (theme, darkTheme) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: theme,
+        initialRoute: '/',
+        onGenerateRoute: RouteGenerator.generateRoute,
+      ),
     );
   }
 }
