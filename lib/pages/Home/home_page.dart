e2f5ebaa06ef779cd10656eef031759c863b0409
Arya <64455337+H-A-L-F@users.noreply.github.com>
2022-09-20 14:58:52 +0700

    popup menu

--- lib/pages/Home/home_page.dart
+++ lib/pages/Home/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -8,6 +9,15 @@ import 'package:myanimex/pages/Home/components/carousel.dart';
 import 'package:myanimex/pages/Home/components/real_home.dart';
 import 'package:myanimex/pages/Items/items_page.dart';
 import 'package:myanimex/pages/Profile/profile_page.dart';
+import 'package:custom_pop_up_menu/custom_pop_up_menu.dart';
+
+class ItemModel {
+  String title;
+  IconData icon;
+  Function() onTap;
+
+  ItemModel(this.title, this.icon, this.onTap);
+}
 
 class HomePage extends StatefulWidget {
   final String email;
@@ -38,13 +48,92 @@ class _HomePageState extends State<HomePage> {
       text: 'Profile',
     ),
   ];
+  final CustomPopupMenuController _controller = CustomPopupMenuController();
+  late List<ItemModel> menuItems;
 
   @override
   Widget build(BuildContext context) {
+    menuItems = [
+      ItemModel(
+        'Switch theme',
+        Icons.sunny,
+        () {
+          AdaptiveTheme.of(context).toggleThemeMode();
+        },
+      ),
+      ItemModel(
+        'Logout',
+        Icons.logout,
+        () {
+          Navigator.of(context).pushNamed('/');
+        },
+      ),
+    ];
+
     return Scaffold(
       appBar: AppBar(
         elevation: 20,
         title: Text(widget.email),
+        actions: <Widget>[
+          CustomPopupMenu(
+            menuBuilder: () => ClipRRect(
+              borderRadius: BorderRadius.circular(5),
+              child: Container(
+                color: const Color(0xFF4C4C4C),
+                child: IntrinsicWidth(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: menuItems
+                        .map(
+                          (item) => GestureDetector(
+                            behavior: HitTestBehavior.translucent,
+                            onTap: () {
+                              _controller.hideMenu();
+                            },
+                            child: Container(
+                              height: 40,
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 20),
+                              child: Row(
+                                children: <Widget>[
+                                  Icon(
+                                    item.icon,
+                                    size: 15,
+                                    color: Colors.white,
+                                  ),
+                                  Expanded(
+                                    child: Container(
+                                      margin: const EdgeInsets.only(left: 10),
+                                      padding: const EdgeInsets.symmetric(
+                                          vertical: 10),
+                                      child: Text(
+                                        item.title,
+                                        style: const TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 12,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        )
+                        .toList(),
+                  ),
+                ),
+              ),
+            ),
+            pressType: PressType.singleClick,
+            verticalMargin: -10,
+            controller: _controller,
+            child: Container(
+              padding: const EdgeInsets.all(20),
+              child: const Icon(Icons.add_circle_outline, color: Colors.white),
+            ),
+          ),
+        ],
       ),
       body: Center(
         child: pages[_selectedIndex],
